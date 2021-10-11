c6946690479931b6a4534f11a3a8ee5803037310
Matt Harvey <mattharvey@google.com>
2021-10-11 13:45:41 -0700

    Removes rate limiting from kupload.sh
    Commit 7123bcb0fcca9e39c35f4b67088b581cdf2fb50d changed the UART driver
    to work around ZMODEM uploads waiting forever on the next rx_watermark
    IRQ that would never come.
    
    With this workaround, we no longer need to rate limit ZMODEM transfers
    on the sender side.
    
    Change-Id: I693dd3bca98f446739a1f01ba537543ffb5dd03e
    GitOrigin-RevId: 1323405d3f5c338e28f5dc4959b72698f4117130

--- kupload.sh
+++ kupload.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 # ZMODEM upload to the Kata rz command over the pty simulating the UART
-#
-# sz is "send ZMODEM", while pv, "pipe viewer" is used for rate limiting.
-#
-# Severe rate limiting is needed to avoid the Renode UART simulation dropping
-# bytes when the RX FIFO is full, since it doesn't provide "hardware" flow
-# control.
 
-sz -O $1 < /tmp/term | pv -L 150 > /tmp/term
+sz -O $1 < /tmp/term > /tmp/term
