fd2a734342aca5ca9de407ae4b326f6ce795558e
Michael Hoang <hoangm@google.com>
2022-07-28 22:29:28 +0000

    Merge "Add smc bin arg to verilator simulator"
    GitOrigin-RevId: fe0781b7ea952e625e49ee34adbe130b899685cd

--- run-chip-verilator-sim.sh
+++ run-chip-verilator-sim.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 # Run verilator testbench simulation.
 
-if [[ $# -lt 4 || $1 == "--help" ]]; then
-  echo "Usage: run-chip-verilator-sim.sh <verilator testbech> <rom binary> <flash binary> <otp binary> [OPTIONS]"
+if [[ $# -lt 5 || $1 == "--help" ]]; then
+  echo "Usage: run-chip-verilator-sim.sh <verilator testbench> <rom binary> <flash binary> <otp binary> <smc binary> [OPTIONS]"
   exit 0
 fi
 
@@ -10,8 +10,9 @@ VCHIP_TB=$1
 ROM_BIN=$2
 FLASH_BIN=$3
 OTP_BIN=$4
+RAM_SMC_BIN=$5
 
-shift 4
+shift 5
 
 if [[ ! -f $(realpath ${VCHIP_TB}) ]]; then
   echo "Verilator testbench not found. Please run \`m matcha_hw_verilator_sim\` or generate the testbench first."
@@ -19,7 +20,7 @@ if [[ ! -f $(realpath ${VCHIP_TB}) ]]; then
 fi
 
 if [[ ! -f $(realpath ${ROM_BIN}) ]] || [[ ! -f $(realpath ${FLASH_BIN}) ]] ||
-   [[ ! -f $(realpath ${OTP_BIN}) ]]; then
+  [[ ! -f $(realpath ${OTP_BIN}) ]] || [[ ! -f $(realpath ${RAM_SMC_BIN}) ]]; then
   echo "Software binaries not found. Please run \`m opentitan_sw_verilator_sim\` or generate the SW binaries first."
   exit 1
 fi
@@ -27,4 +28,5 @@ fi
 ${VCHIP_TB} \
   "--meminit=rom,${ROM_BIN}" \
   "--meminit=flash,${FLASH_BIN}" \
-  "--meminit=otp,${OTP_BIN}" $@
+  "--meminit=otp,${OTP_BIN}" \
+  "--meminit=ram_smc,${RAM_SMC_BIN}" $@
